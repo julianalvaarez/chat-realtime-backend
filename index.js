eb224fbe0687f258117dd1e6dc0c7b886bd9583f
julianalvaarez <julialva2008@gmail.com>
2024-01-18 23:15:16 -0300

    v1.3

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 import express from 'express'
 import http from 'http'
-import cors from 'cors'
 // Importa el módulo Server de socket.io para gestionar conexiones WebSocket
 import {Server as SocketServer} from 'socket.io'
 
 const PORT = process.env.PORT ?? 3000
 const app = express()
-app.use(cors())
 const server = http.createServer(app) // Crea un servidor HTTP usando Express
-const io = new SocketServer(server) // Crea una instancia de SocketServer asociada al servidor HTTP
+const io = new SocketServer(server, {
+    cors: {
+        origin: 'https://localhost:5173'
+    }
+}) // Crea una instancia de SocketServer asociada al servidor HTTP
 
 
 // Maneja el evento de cuando alguien se conecta en el servidor de socket.io
 io.on('connection', socket => {
+    console.log('Client Connected')
     // Maneja el evento 'message' cuando un cliente envía un mensaje
     socket.on('message', (messageData) => {
         // Emite el mensaje a todos los clientes excepto al remitente
